f90f6fa68f267343fe84544b8058aae121838960
Max Layefa Kenneth <Kennethlayefamax@gmail.com>
2024-08-31 19:24:25 +0100

    Cybernate website updated codes by Max

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Navbar from './Components/Navbar/Navbar';
-import Sidebar from './Components/Sidebar';
+import Sidebar from './Components/Sidebar/Sidebar';
 import Home from './Components/Home/Home';
 import AboutUs from './Components/AboutUs/AboutUs';
 import Contact from './Components/Contact/Contact';
 import FAQ from './Components/FAQ/FAQ';
 import Services from './Components/Services/Services';
 import Solutions from './Components/Solutions/Solutions';
+import Slider from './Components/Slider/Slider';
 
 function App() { 
+
+  const [isSidebarOpen, SetIsSidebarOpen] = useState(false);
+
+  const openSidebarHandle = () => {
+    SetIsSidebarOpen(!isSidebarOpen);
+  }
+
+  const closeSidebarHandle = () => {
+    SetIsSidebarOpen(isSidebarOpen);
+  }
+  
   return (
     <div className="App">
-      <Navbar />
-      <Sidebar />
-      <Home />
-      <AboutUs />
-      <Contact />
-      <FAQ />
-      <Services />
-      <Solutions />
+      <Navbar openSidebarHandle={openSidebarHandle}/>
+      <Slider />
+      <Sidebar isSidebarOpen={isSidebarOpen} closeSidebarHandle={closeSidebarHandle} />
     </div>
   );
 }
